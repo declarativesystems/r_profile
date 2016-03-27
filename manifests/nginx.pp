bc9971b77f781b6c2addd0de16bb8ff965f4917e
Geoff Williams <geoff.williams@puppetlabs.com>
2016-03-28 03:06:28 +1100

    default virtual host for nginx

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -1,6 +1,26 @@
 class r_profile::nginx {
   class { "::nginx": }
 
+  $www_root = "/var/www/nginx-default"
+
+  #
+  # Default virtual host
+  #
+  nginx::resource::vhost { $fqdn:
+    listen_options  => "default_server",
+    www_root        => $www_root,
+  }
+
+  File {
+    owner => "root",
+    group => "root",
+    mode  => "0755",
+  } 
+
+  file { $www_root:
+    ensure => directory,
+  }
+
   #  nginx::resource::upstream { 'lsdserver':
   #    members => [
   #      'localhost:3000',
