a2178a2c3bd29991a53f2cf1a904332c58c58e20
Geoff Williams <geoff.williams@puppetlabs.com>
2016-07-13 16:32:12 +1000

    better default motd

--- manifests/motd.pp
+++ manifests/motd.pp
@@ -6,7 +6,7 @@
 # *inline_template*
 #   string to be processed as an inline template for the MOTD
 class r_profile::motd(
-    $template     = hiera("r_profile::motd::template", undef),
+    $template     = hiera("r_profile::motd::template", "${module_name}/motd.erb"),
     $content      = hiera("r_profile::motd::content", undef),
     $dynamic_motd = hiera("r_profile::motd::content",true),
 ) {
