7bf20498cb42980ec53b7bf2f054074510eab4c3
Geoff Williams <geoff.williams@puppetlabs.com>
2016-07-15 00:20:11 +1000

    different proxy setup on windows

--- manifests/puppet/agent.pp
+++ manifests/puppet/agent.pp
@@ -15,59 +15,80 @@ class r_profile::puppet::agent(
       enable => true,
     }
 
-    file { $sysconf_puppet:
-      ensure => file,
-      owner  => "root",
-      group  => "root",
-      mode   => "0644",
-    }
-
-    # restart agent service if any file_line resources change it.  On linux
-    # systems we also need to tell systemctl to reload itself
     if $kernel == "windows" {
       $puppet_agent_notifications = Service[$puppet_agent_service]
+ 
+      registry_value { 'http_proxy':
+        ensure => $proxy,
+        type   => "string",
+        data   => $proxy,
+        key    => 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment\http_proxy',
+      }
+      
+      registry_value { 'https_proxy':
+        ensure => $proxy,
+        type   => "string",
+        data   => $proxy,
+        key    => 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment\https_proxy',
+      }
+      
+      reboot { "after_proxy_set":
+        subscribe => [
+          Registry_value["http_proxy"],
+          Registry_value["https_proxy"],
+        ],
+      }
     } else {
+ 
+      file { $sysconf_puppet:
+        ensure => file,
+        owner  => "root",
+        group  => "root",
+        mode   => "0644",
+      }
+
       $puppet_agent_notifications = [
         Exec["systemctl_daemon_reload"],
         Service[$puppet_agent_service],
       ]
-    }
-    File_line <| path == $sysconf_puppet |> ~> $puppet_agent_notifications
+    
+      File_line <| path == $sysconf_puppet |> ~> $puppet_agent_notifications
 
-    #
-    # Proxy server monkey patching
-    #
-    if $proxy {
-      $regexp = 'https?://(.*?@)?([^:]+):(\d+)'
-      $proxy_host = regsubst($proxy, $regexp, '\2')
-      $proxy_port = regsubst($proxy, $regexp, '\3')
-      if $export_variable {
-        # solaris needs a 2-step export
-        $http_proxy_var   = "http_proxy=${proxy}; export http_proxy"
-        $https_proxy_var  = "https_proxy=${proxy}; export https_proxy"
+      #
+      # Proxy server monkey patching
+      #
+      if $proxy {
+        $regexp = 'https?://(.*?@)?([^:]+):(\d+)'
+        $proxy_host = regsubst($proxy, $regexp, '\2')
+        $proxy_port = regsubst($proxy, $regexp, '\3')
+        if $export_variable {
+          # solaris needs a 2-step export
+          $http_proxy_var   = "http_proxy=${proxy}; export http_proxy"
+          $https_proxy_var  = "https_proxy=${proxy}; export https_proxy"
+        } else {
+          $http_proxy_var   = "http_proxy=${proxy}"
+          $https_proxy_var  = "https_proxy=${proxy}"
+        }
       } else {
-        $http_proxy_var   = "http_proxy=${proxy}"
-        $https_proxy_var  = "https_proxy=${proxy}"
+        # nasty hack - we MUST have two different space permuations here or 
+        # file_line will only remove a single entry as it has already matched 
+        $http_proxy_var  = " "
+        $https_proxy_var = "  " 
       }
-    } else {
-      # nasty hack - we MUST have two different space permuations here or 
-      # file_line will only remove a single entry as it has already matched 
-      $http_proxy_var  = " "
-      $https_proxy_var = "  " 
-    }
 
-    file_line { "puppet agent http_proxy":
-      ensure => present,
-      path   => $sysconf_puppet,
-      line   => $http_proxy_var,
-      match  => "http_proxy=",
-    }
+      file_line { "puppet agent http_proxy":
+        ensure => present,
+        path   => $sysconf_puppet,
+        line   => $http_proxy_var,
+        match  => "http_proxy=",
+      }
 
-    file_line { "puppet agent https_proxy":
-      ensure => present,
-      path   => $sysconf_puppet,
-      line   => $https_proxy_var,
-      match  => "https_proxy=",
+      file_line { "puppet agent https_proxy":
+        ensure => present,
+        path   => $sysconf_puppet,
+        line   => $https_proxy_var,
+        match  => "https_proxy=",
+      }
     }
   }
 }
