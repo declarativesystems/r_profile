ee03f19bb2842c890b0292832f2c17c0eacb83a4
Geoff Williams <geoff.williams@puppetlabs.com>
2016-07-22 22:38:25 +1000

    empty groups is mandatory

--- manifests/linux/sudo.pp
+++ manifests/linux/sudo.pp
@@ -14,6 +14,7 @@ class r_profile::linux::sudo {
     user { "vagrant":
       ensure      => present,
       gid         => "vagrant",
+      groups      => [],
       membership  => inclusive,
     }
   }
