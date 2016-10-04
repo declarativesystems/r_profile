b0375954fdb144887cf12cc9b10488a2351021bd
Geoff Williams <geoff.williams@puppetlabs.com>
2016-10-04 22:35:38 +1100

    firewall and monitoring for wordpress

--- manifests/wordpress.pp
+++ manifests/wordpress.pp
@@ -1,18 +1,36 @@
-class r_profile::wordpress {
-    class { 'mysql::server':
-        root_password => 'puppetlabs',
-    }
-    class { 'mysql::bindings':
-        php_enable => true,
-    }
+class r_profile::wordpress(
+    $nagios_monitored = true,
+    $enable_firewall  = true,
+) {
+  class { 'mysql::server':
+    root_password => 'puppetlabs',
+  }
+  class { 'mysql::bindings':
+    php_enable => true,
+  }
 
-    include apache
-    include apache::mod::php
-    apache::vhost { $::fqdn:
-        port     => '80',
-        priority => '00',
-        docroot  => '/opt/wordpress',
-    }
+  include apache
+  include apache::mod::php
+  apache::vhost { $::fqdn:
+    port     => '80',
+    priority => '00',
+    docroot  => '/opt/wordpress',
+  }
 
-    include ::wordpress
+  include ::wordpress
+
+  if $nagios_monitored {
+    nagios::nagios_service_http { 'wordpress':
+      port => $port,
+      url  => '/',
+    }
+  }
+
+  if $enable_firewall and !defined(Firewall["100 ${::fqdn} HTTP ${port}"]) {
+    firewall { "100 ${::fqdn} HTTP ${port}":
+      dport   => 80,
+      proto   => 'tcp',
+      action  => 'accept',
+    }
+  }
 }
