821edf97b4724ce62b2e44d49e635377c3269d5e
Geoff Williams <geoff.williams@puppetlabs.com>
2016-10-04 22:25:19 +1100

    fixup

--- manifests/nagios_server.pp
+++ manifests/nagios_server.pp
@@ -1,17 +1,8 @@
 class r_profile::nagios_server(
-    4$password = hiera("r_profile::nagios_server::password", "nagios"),
-    
+    $password = hiera("r_profile::nagios_server::password", "nagios"),
 ) {
   include nagios
   class { "nagios::server":
     password => $password,
-  }
-
-  if $enable_firewall and !defined(Firewall["100 ${::fqdn} HTTP ${website_port}"]) {
-    firewall { "100 ${::fqdn} HTTP ${port}":
-      dport   => $port,
-      proto   => 'tcp',
-      action  => 'accept',
-    }
   }
 }
