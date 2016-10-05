69c11fa4d730a740f1e52138876565a036e089c8
Geoff Williams <geoff.williams@puppetlabs.com>
2016-10-05 17:36:09 +1100

    firewall and monitoring for mysql

--- manifests/database/mysql_server.pp
+++ manifests/database/mysql_server.pp
@@ -1,9 +1,15 @@
 class r_profile::database::mysql_server(
     $root_password    = hiera("r_profile::database::mysql_server::root_password", 'changeme'),
-    $override_options = undef,
+    $override_options = hiera("r_profile::database::mysql_server::override_options", undef),
     $db               = hiera("r_profile::database::mysql_server::db", {}),
     $db_default       = hiera("r_profile::database::mysql_server::db_default", {}),
+    $nagios_monitored = hiera("r_profile::database::mysql_server::nagios_monitored", true),
+    $enable_firewall  = hiera("r_profile::database::mysql_server::enable_firewall", true),
 ) {
+ 
+  # always 3306
+  $port = 3306
+
   class { '::mysql::server':
     root_password           => $root_password,
     remove_default_accounts => true,
@@ -11,4 +17,19 @@ class r_profile::database::mysql_server(
   }
 
   create_resources("mysql::db", $db, $db_default)
+
+  if $nagios_monitored {
+    nagios::nagios_service_tcp { 'PE PCP/PXP':
+      port => $port,
+    }
+  }
+
+  if $enable_firewall and !defined(Firewall["100 ${::fqdn} TCP ${port}"]) {
+    firewall { "100 ${::fqdn} TCP ${port}":
+      dport   => $port,
+      proto   => 'tcp',
+      action  => 'accept',
+    }
+  }
+
 }
