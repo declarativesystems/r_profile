c3eda69dbbcffd9a267c6f4401de2e6ac4d9d144
Geoff Williams <geoff.williams@puppetlabs.com>
2016-10-05 15:04:01 +1100

    fixup names and firewalls, do in tomcat layer

--- manifests/web_service/tomcat.pp
+++ manifests/web_service/tomcat.pp
@@ -1,9 +1,13 @@
 class r_profile::web_service::tomcat(
-  $source_url     = 'http://apache.mirror.digitalpacific.com.au/tomcat/tomcat-8/v8.5.5/bin/apache-tomcat-8.5.5.tar.gz',
-  $catalina_home  = '/opt/tomcat',
-  $service        = 'tomcat',
-  $user           = 'tomcat',
-  $group          = 'tomcat',
+    $source_url       = 'http://apache.mirror.digitalpacific.com.au/tomcat/tomcat-8/v8.5.5/bin/apache-tomcat-8.5.5.tar.gz',
+    $catalina_home    = '/opt/tomcat',
+    $service          = 'tomcat',
+    $user             = 'tomcat',
+    $group            = 'tomcat',
+    $port             = 8080,
+    $lb               = true,
+    $nagios_monitored = true,
+    $enable_firewall  = true,
 ){
 
   include java
@@ -22,4 +26,52 @@ class r_profile::web_service::tomcat(
     require => Tomcat::Install[$catalina_home]
   }
 
+  if $lb {
+
+    # setup the FACT that will tell us what IP address to use (run n)
+    if is_string($lb) {
+      $lb_address = $lb
+    } else {
+      # attempt to lookup which nodes are classified as Haproxies and use first
+      $lb_addresses = query_nodes('Class[R_profile::Monitor::Haproxy]')
+      if is_array($lb_addresses) {
+        $lb_address = $lb_addresses[0]
+      } else {
+        $lb_address = false
+      }
+    }
+
+    if $lb_address and is_string($lb) {
+      source_ipaddress{ $lb_address: }
+      $source_ip = $source_ipaddress[$lb_address]
+    } else {
+      $source_ip = undef
+    }
+
+    # export the IP address (run n+1)
+    @@haproxy::balancermember { "${service_name}-${::fqdn}":
+      listening_service => 'tomcat',
+      server_names      => $::fqdn,
+      ipaddresses       => $source_ip,
+      ports             => $port,
+      options           => 'check',
+    }
+
+    # runs will be collected on the loadbalancer next time it runs puppet
+  }
+
+  if $nagios_monitored {
+    nagios::nagios_service_http { 'tomcat':
+      port => $port,
+      url  => '/',
+    }
+  }
+
+  if $enable_firewall and !defined(Firewall["100 ${::fqdn} HTTP ${port}"]) {
+    firewall { "100 ${::fqdn} HTTP ${port}":
+      dport   => $port,
+      proto   => 'tcp',
+      action  => 'accept',
+    }
+  }
 }
--- manifests/webapp/geoserver.pp
+++ manifests/webapp/geoserver.pp
@@ -4,8 +4,6 @@ class r_profile::webapp::geoserver(
   $lb               = true,
   $service_name     = 'geoserver',
   $nagios_monitored = true,
-  $enable_firewall  = true,
-  $port             = 8080,
 ) {
 
   # tomcat
@@ -100,40 +98,6 @@ class r_profile::webapp::geoserver(
     require     => Archive[$war_file],
   }
 
-  if $lb {
-
-    # setup the FACT that will tell us what IP address to use (run n)
-    if is_string($lb) {
-      $lb_address = $lb
-    } else {
-      # attempt to lookup which nodes are classified as Haproxies and use first
-      $lb_addresses = query_nodes('Class[R_profile::Monitor::Haproxy]')
-      if is_array($lb_addresses) {
-        $lb_address = $lb_addresses[0]
-      } else {
-        $lb_address = false
-      }
-    }
-
-    if $lb_address and is_string($lb) {
-      source_ipaddress{ $lb_address: }
-      $source_ip = $source_ipaddress[$lb_address]
-    } else {
-      $source_ip = undef
-    }
-
-    # export the IP address (run n+1)
-    @@haproxy::balancermember { "${service_name}-${::fqdn}":
-      listening_service => $service_name,
-      server_names      => $::fqdn,
-      ipaddresses       => $source_ip,
-      ports             => 8080,
-      options           => 'check',
-    }
-
-    # runs will be collected on the loadbalancer next time it runs puppet
-  }
-
   if $nagios_monitored {
     nagios::nagios_service_http { 'geoserver':
       port => $port,
@@ -141,11 +105,4 @@ class r_profile::webapp::geoserver(
     }
   }
 
-  if $enable_firewall and !defined(Firewall["100 ${::fqdn} HTTP ${port}"]) {
-    firewall { "100 ${::fqdn} HTTP ${port}":
-      dport   => $port,
-      proto   => 'tcp',
-      action  => 'accept',
-    }
-  }
 }
