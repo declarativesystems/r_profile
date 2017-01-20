1aa05b9cdf381a744b97d41aedbafcf80bdbb342
Geoff Williams <geoff.williams@puppetlabs.com>
2017-01-20 20:32:37 +1100

    no firewall changes by default and option to do nothing

--- manifests/linux/firewall.pp
+++ manifests/linux/firewall.pp
@@ -1,17 +1,24 @@
-class r_profile::linux::firewall($enable=true) {
+# R_profile::Linux::Firewall
+#
+# Management of the IPtables Linux firewall
+#
+# @param ensure 'managed' take control of the IPtables firewall, 'disabled' turn IPtables off, 'unmanaged' do not change firewall settings
+class r_profile::linux::firewall(
+    Enum['managed', 'disabled', 'unmanaged'] $ensure = hiera('r_profile::linux::firewall::ensure', 'unmanaged'),
+) {
   # Suggested global firewall defaults declared in r10k-control/manifests/site.pp
   #Firewall {
   #  before  => Class['profile::fw::post'],
   #  require => Class['profile::fw::pre'],
   #}
 
-  if $enable {
+  if $ensure == 'managed' {
     include firewall
     include r_profile::fw::pre
     include r_profile::fw::post
-  } else {
+  } elsif $ensure == 'disabled' {
     class { 'firewall':
       ensure => stopped,
     }
-  }
+  } # else unmanaged, so do nothing
 }
