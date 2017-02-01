559f8d708804752c2f17407de010680b365a0dad
Geoff Williams <geoff.williams@puppetlabs.com>
2017-02-01 20:58:53 +0800

    document param

--- manifests/linux/selinux.pp
+++ manifests/linux/selinux.pp
@@ -6,7 +6,9 @@
 #   leave the current SELinux mode alone, the remaining allowed values force the
 #   SELinux mode to be enforced as requested.  Don't forget that you usually have
 #   to reboot after changing the mode.  To avoid accidentally rebooting systems
-#   this module does not do this for you. 
+#   this module does not do this for you.
+# @param remove_troubleshoot true to remove the setroubleshoot package, false to
+#   leave it alone
 class r_profile::linux::selinux(
     Enum['noop', 'enforcing', 'permissive', 'disabled'] $sel_mode =
       hiera("r_profile::linux::selinux::mode", 'noop'),
