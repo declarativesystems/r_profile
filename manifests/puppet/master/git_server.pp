1c2e653bf0552d0e9e0341add7d2fec2dd0c6cf1
Geoff Williams <geoff.williams@puppetlabs.com>
2017-02-01 14:17:52 +0800

    fix partial refactor

--- manifests/puppet/master/git_server.pp
+++ manifests/puppet/master/git_server.pp
@@ -1,7 +1,14 @@
+# R_profile::Puppet::Master::Git_server
+#
+# Enable a simple built-in Git Server for puppet by creating a bare git
+# repository accessed via SSH and configuring Code Manager to use it
+#
+# @param enable true to enable the git server, otherwise false to do nothing
+
 class r_profile::puppet::master::git_server(
-  Boolean $install = hiera('r_profile::puppet::master::install', true),
+  Boolean $enable = hiera('r_profile::puppet::master::git_server::enable', true),
 ) {
-  if $install {
+  if $enable {
     include psquared::git
   }
 }
