8bb2a1513bf35f8b088a17c1e5cf50063d4a08f9
Geoff Williams <geoff.williams@puppetlabs.com>
2017-02-02 15:05:39 +0800

    support for solaris and aix lockdown of at

--- manifests/lockdown/at.pp
+++ manifests/lockdown/at.pp
@@ -7,14 +7,32 @@ class r_profile::lockdown::at(
   Boolean $ensure = hiera('r_profile::lockdown::at::ensure', false),
 ) {
 
+  case $facts['os']['family'] {
+    "RedHat": {
+      $at_deny  = '/etc/at.deny'
+      $at_allow = '/etc/at.allow'
+    }
+    "AIX": {
+      $at_deny  = "/var/adm/cron/at.deny"
+      $at_allow = "/var/adm/cron/at.allow"
+    }
+    "Solaris": {
+      $at_deny  = "/etc/cron.d/at.deny"
+      $at_allow = "/etc/cron.d/at.allow"
+    }
+    default:{
+      fail("Class ${name} does not support ${facts['os']['family']} yet")
+    }
+  }
+
   if $ensure {
-    file { '/etc/at.deny':
+    file { $at_deny:
       ensure => absent,
     }
 
     # by creating this file, only users listed (and root!) will be able to
     # schedule at jobs
-    file { '/etc/at.allow':
+    file { $at_allow:
       ensure => present,
       owner  => 'root',
       group  => 'root',
--- spec/classes/lockdown/at_spec.rb
+++ spec/classes/lockdown/at_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 describe 'r_profile::lockdown::at' do
+  let :facts do
+    {
+      :os => {
+        :family => 'RedHat',
+      }
+    }
+  end
 
   context 'with default values for all parameters' do
     it { should contain_class('r_profile::lockdown::at') }
@@ -18,6 +25,40 @@ describe 'r_profile::lockdown::at' do
   context 'leaves files alone when inactive' do
     it { should_not contain_file('/etc/at.allow')}
     it { should_not contain_file('/etc/at.deny')}
+  end
+
+  context 'manages correct files on solaris' do
+    let :facts do
+      {
+        :os => {
+          :family => 'Solaris',
+        }
+      }
+    end
+    let :params do
+      {
+        :ensure => true,
+      }
+    end
+    it { should contain_file('/etc/cron.d/at.allow')}
+    it { should contain_file('/etc/cron.d/at.deny')}
+  end
+
+  context 'manages correct files on aix' do
+    let :facts do
+      {
+        :os => {
+          :family => 'Aix',
+        }
+      }
+    end
+    let :params do
+      {
+        :ensure => true,
+      }
+    end
+    it { should contain_file('/var/adm/cron/at.allow')}
+    it { should contain_file('/var/adm/cron/at.deny')}
   end
 
 end
