8491ee6b64b3c605ae059433e3d925d6c9e390a5
Geoff Williams <geoff.williams@puppetlabs.com>
2017-02-06 21:47:40 +0800

    pass-tru issue.net and issue

--- manifests/motd.pp
+++ manifests/motd.pp
@@ -10,14 +10,18 @@
 # *inline_template*
 #   string to be processed as an inline template for the MOTD
 class r_profile::motd(
-    $template     = hiera("r_profile::motd::template", "${module_name}/motd.erb"),
-    $content      = hiera("r_profile::motd::content", undef),
-    $dynamic_motd = hiera("r_profile::motd::content",true),
+    $template           = hiera("r_profile::motd::template", "${module_name}/motd.erb"),
+    $content            = hiera("r_profile::motd::content", undef),
+    $issue_content      = hiera("r_profile::motd::issue_content", undef),
+    $issue_net_content  = hiera("r_profile::motd::issue_net_content", undef),
+    $dynamic_motd       = hiera("r_profile::motd::dynamic_motd",true),
 ) {
 
   class { "::motd":
-    template     => $template,
-    content      => $content,
-    dynamic_motd => $dynamic_motd,
+    template          => $template,
+    content           => $content,
+    dynamic_motd      => $dynamic_motd,
+    issue_content     => $issue_content,
+    issue_net_content => $issue_net_content,
   }
 }
