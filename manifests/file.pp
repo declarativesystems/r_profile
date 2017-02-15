0fb2a1d9b673b3473f38a8d4d6d170a463c89601
Geoff Williams <geoff.williams@puppetlabs.com>
2017-02-15 09:52:37 +0800

    doco, release

--- manifests/file.pp
+++ manifests/file.pp
@@ -1,3 +1,9 @@
+# R_profile::File
+#
+# Support for managing files and directories
+#
+# @param files Hash of files to create (suitable for create_resources)
+# @param directories Hash of directories to create (suitable for create_resources)
 class r_profile::file(
   $files        = hiera("r_profile::file::files", false),
   $directories  = hiera("r_profile::file::directories", false),
