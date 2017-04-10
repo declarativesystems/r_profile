34f686301155ce394b60c660dd9b1c72a14d9d8f
Geoff Williams <geoff@geoffwilliams.me.uk>
2017-04-10 12:11:57 +1000

    dont take over dns unless told to

--- manifests/dns_client.pp
+++ manifests/dns_client.pp
@@ -9,22 +9,28 @@
 # @param nameservers Array of nameservers to use
 # @param options Options Array of options to use
 # @param nss_entries Hash of settings to add to the nsswitch.conf file
+# @param install_defaults True to install module default DNS/NSS settings if
+#   nothing found in hiera, otherwise dont' reconfigure dns
 class r_profile::dns_client(
-    $search       = hiera("r_profile::dns_client::search", undef),
-    $nameservers  = hiera("r_profile::dns_client::nameservers", undef),
-    $options      = hiera("r_profile::dns_client::options", undef),
-    $nss_entries  = hiera("r_profile::dns_client::nss_entries", undef),
+    $search           = hiera("r_profile::dns_client::search", undef),
+    $nameservers      = hiera("r_profile::dns_client::nameservers", undef),
+    $options          = hiera("r_profile::dns_client::options", undef),
+    $nss_entries      = hiera("r_profile::dns_client::nss_entries", undef),
+    $install_defaults = hiera("r_profile::dns_client::install_defaults", false),
 ) {
 
   # setup /etc/resolve.conf
-  class { "resolv_conf":
-    search      => $search,
-    nameservers => $nameservers,
-    options     => $options,
+  if $nameservers or $install_defaults {
+    class { "resolv_conf":
+      search      => $search,
+      nameservers => any2array($nameservers),
+      options     => $options,
+    }
   }
 
-  class { "name_service_switch":
-    entries => $nss_entries,
+  if $nss_entries or $install_defaults {
+    class { "name_service_switch":
+      entries => $nss_entries,
+    }
   }
-
 }
--- spec/classes/dns_client_spec.rb
+++ spec/classes/dns_client_spec.rb
@@ -13,4 +13,50 @@ describe 'r_profile::dns_client' do
   context 'with default values for all parameters' do
     it { should contain_class('r_profile::dns_client') }
   end
+
+  context 'does not change dns settings when no servers specified ' do
+    let :params do
+      {
+      }
+    end
+    it { should_not contain_class('resolv_conf') }
+  end
+
+  context 'changes dns settings when no servers specified ' do
+    let :params do
+      {
+        :nameservers => '1.1.1.1',
+      }
+    end
+    it { should contain_class('resolv_conf') }
+  end
+
+  context 'changes dns settings to module defaults when asked to' do
+    let :params do
+      {
+        :install_defaults => true,
+      }
+    end
+    it { should contain_class('resolv_conf') }
+    it { should contain_class('name_service_switch') }
+  end
+
+  context 'does not change nss settings when no keys specified ' do
+    let :params do
+      {
+      }
+    end
+    it { should_not contain_class('name_service_switch') }
+  end
+
+  context 'changes nss settings when keys specified ' do
+    let :params do
+      {
+        :nss_entries => {'hosts' => 'dns files'},
+      }
+    end
+    it { should contain_class('name_service_switch') }
+  end
+
+
 end
