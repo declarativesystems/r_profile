996ada24f66ceef5777b8cf609b5088c71197898
Geoff Williams <geoff@geoffwilliams.me.uk>
2017-04-20 22:27:08 +1000

    fix renamed class in base profile; release

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,10 +1,10 @@
 # R_profile::Base
 #
-# A generic 'baseleve' style class 
+# A generic 'baseleve' style class
 class r_profile::base {
   include r_profile::motd
   include r_profile::software
-  include r_profile::users
+  include r_profile::user
   include "r_profile::${downcase($kernel)}::base"
   include r_profile::monitor::nagios_monitored
 }
