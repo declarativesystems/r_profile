07343e02340a62b037959bd7e82ccaaabdab9e8f
Geoff Williams <geoff@geoffwilliams.me.uk>
2017-04-20 23:18:03 +1000

    fixed renamed ntp class

--- manifests/linux/base.pp
+++ manifests/linux/base.pp
@@ -6,7 +6,7 @@ class r_profile::linux::base {
   include r_profile::linux::vim
   include r_profile::linux::sudo
   include r_profile::linux::systemd
-  include r_profile::linux::ntp
+  include r_profile::ntp
   include r_profile::linux::puppet_agent
   include r_profile::linux::package_manager
 }
