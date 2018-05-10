4656847c272d56ef317eaa0d87c6eb064eb3fba3
Geoff Williams <geoff@geoffwilliams.me.uk>
2018-05-10 15:41:31 +1000

    support for bind mounting /var/tmp, added nodev to list of mount flags

--- manifests/linux/mounts.pp
+++ manifests/linux/mounts.pp
@@ -1,12 +1,20 @@
 # Redhat_tidy::Mounts
 #
-# Common mount good practices for RedHat
-class r_profile::linux::mounts {
+# Common mount good practices for RedHat:
+#   * `/tmp` mounted `nodev,nosuid,noexec` (if separate partition)
+#   * `/dev/shm` mounted `nodev,nosuid,noexec` (if separate partition)
+#   * `/home` mounted `nodev` (if separate partition)
+#   * Optionally bind mount `/var/tmp` to `/tmp`
+#
+# @param bind_mount_var_tmp `true` to bind mount `/var/tmp` to `/tmp`
+class r_profile::linux::mounts(
+    Boolean $bind_mount_var_tmp = false,
+) {
   if dig($facts, 'mountpoints', '/tmp') {
     mount { '/tmp':
       ensure  => mounted,
       atboot  => true,
-      options => 'nosuid,noexec',
+      options => 'nodev,nosuid,noexec',
       device  => $facts['mountpoints']['/tmp']['device'],
       fstype  => $facts['mountpoints']['/tmp']['filesystem'],
     }
@@ -19,6 +27,29 @@ class r_profile::linux::mounts {
       options => 'nodev,nosuid,noexec',
       device  => $facts['mountpoints']['/dev/shm']['device'],
       fstype  => $facts['mountpoints']['/dev/shm']['filesystem'],
+    }
+  }
+
+  if dig($facts, 'mountpoints', '/home') {
+    mount { '/home':
+      ensure  => mounted,
+      atboot  => true,
+      options => 'nodev',
+      device  => $facts['mountpoints']['/home']['device'],
+      fstype  => $facts['mountpoints']['/home']['filesystem'],
+    }
+  }
+
+  if $bind_mount_var_tmp {
+    file { "/var/tmp":
+      ensure => directory,
+      owner  => root,
+    }
+    mount { '/var/tmp':
+      ensure  => mounted,
+      device  => '/tmp',
+      fstype  => 'none',
+      options => 'bind,nodev,nosuid,noexec',
     }
   }
 
