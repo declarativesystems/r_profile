661ceb45b9373c4739e2b186d0c51171b88572a0
Geoff Williams <geoff@geoffwilliams.me.uk>
2018-06-04 11:58:54 +1000

    wip samba server

---
+++ manifests/linux/samba_server.pp
@@ -0,0 +1,31 @@
+# WIP easy samba support
+class r_profile::linux::samba_server {
+
+  package { ["samba", "samba-common-bin"]:
+    ensure => present,
+  }
+
+  file { "/shared":
+    ensure => directory,
+    owner  => "root",
+    group  => "root",
+    mode   => "1777",
+  }
+
+  service { "samba":
+    ensure => running,
+    enable => true,
+  }
+  # sudo smbpasswd -a pi
+  #
+  # [share]
+  # Comment = Pi shared folder
+  # Path = /share
+  # Browseable = yes
+  # Writeable = Yes
+  # only guest = no
+  # create mask = 0777
+  # directory mask = 0777
+  # Public = yes
+  # Guest ok = yes
+}
