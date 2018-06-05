4c3a9c87cc4063ec89051ff263425db88718e051
Geoff Williams <geoff@geoffwilliams.me.uk>
2018-06-05 15:25:31 +1000

    fixedup firewalld support, tests for samba, remove pdqtest puppetfile and add to gitignore

--- manifests/linux/firewalld.pp
+++ manifests/linux/firewalld.pp
@@ -1,30 +1,71 @@
-# R_Profile::Linux::Firewalld
+# R_profile::Linux::Firewalld
 #
 # Manage firewalld using crayfishx-firewalld. Accepts base plus additional
 # options for each set of rule resources being managed for easy managment
-# using hiera. At runtime these will be run in order (base first).
+# using hiera.
 #
-# @example hiera data
-#   firewalld
+# @see https://forge.puppet.com/crayfishx/firewalld
 #
-# @param enable True to manage firewalld rules, false to leave as-is
-# @param base_firewalld_service base level `firewalld_service` rules
-#   as a hash of options
-# @param firewalld_service additional `firewalld_service` rules
-#   as a hash of options
-# @param base_firewalld_rich_rule base level `firewalld_rich_rule` rules
-#   as a hash of options
-# @param base_firewalld_rich_rule additional `firewalld_rich_rule` rules
-#   as a hash of options
+# @example hiera data for enabling free access to a service
+#   r_profile::linux::firewalld::firewalld_service:
+#     "allow ssh":
+#       ensure: present
+#       service: ssh
+#     "allow puppetmaster":
+#       ensure: present
+#       service: "puppetmaster"
+#     "allow puppet orchestrator":
+#       ensure:  present
+#       service: "puppet-orchestrator"
+#     "allow https":
+#       ensure: present
+#       service: "https"
+#
+# @example hiera data for creating a rich rule
+#   r_profile::linux::firewalld::firewalld_rich_rule:{}
+#     'allow access to tomcat from internal':
+#       ensure: present
+#       zone: public
+#       action: accept
+#       source: '192.168.1.0/24'
+#       port:
+#         port: 8080
+#         protocol: tcp
+#
+# @example disable firewalld
+#   r_profile::linux::firewalld::service_ensure: stopped
+#   r_profile::linux::firewalld::service_enable: false
+#
+# @param service_ensure How to ensure the `firewalld` service
+# @param service_enable `true` to start firewalld on boot otherwise `false` to disable it
+# @param base_firewalld_service base level `firewalld_service` rules (see examples)
+# @param firewalld_service additional `firewalld_service` rules (see examples)
+# @param base_firewalld_rich_rule base level `firewalld_rich_rule` rules (see examples)
+# @param base_firewalld_rich_rule additional `firewalld_rich_rule` rules (see examples)
 class r_profile::linux::firewalld(
-  Boolean     $enable                   = true,
-  Array[Hash] $base_firewalld_service   = [],
-  Array[Hash] $firewalld_service        = [],
-  Array[Hash] $base_firewalld_rich_rule = [],
-  Array[Hash] $firewalld_rich_rule      = [],
+    Enum['running', 'stopped']    $service_ensure           = 'running',
+    Boolean                       $service_enable           = true,
+    Hash[String, Optional[Hash]]  $base_firewalld_service   = {},
+    Hash[String, Optional[Hash]]  $firewalld_service        = {},
+    Hash[String, Optional[Hash]]  $base_firewalld_rich_rule = {},
+    Hash[String, Optional[Hash]]  $firewalld_rich_rule      = {},
 ) {
-  if $enable {
-    class { "firewalld": }
+
+  # Service definition for Puppet Orchestrator (8142) - PuppetMaster definition alreaady ships with RHEL7
+  firewalld::custom_service{ "puppet-orchestrator":
+    description => 'Puppet Orchestrator/PXP runs actions on agents via the Puppet Execution Protocol agent.',
+    port        => [{
+      'port'     => '8142',
+      'protocol' => 'tcp',
+    }],
+  }
+
+  class { "firewalld":
+    service_ensure => $service_ensure,
+    service_enable => $service_enable,
+  }
+
+  if $service_ensure == 'running' {
     firewalld_zone { "public":
       ensure           => "present",
       target           => "default",
@@ -34,27 +75,23 @@ class r_profile::linux::firewalld(
       purge_ports      => true,
     }
 
-    ($firewalld_service + $base_firewalld_service).each |$rule| {
-      $rule.each |$key, $opts| {
-        firewalld_service {
-          default:
-            ensure => present,
-          ;
-          $key:
-            * => $opts,
-        }
+    merge($base_firewalld_service, $firewalld_service).each |$key, $opts| {
+      firewalld_service {
+        default:
+          ensure => present,
+        ;
+        $key:
+          * => $opts,
       }
     }
 
-    ($firewalld_rich_rule + $base_firewalld_rich_rule).each |$rule| {
-      $rule.each |$key, $opts| {
-        firewalld_rich_rule {
-          default:
-            ensure => present,
-          ;
-          $key:
-            * => $opts,
-        }
+    merge($base_firewalld_rich_rule, $firewalld_rich_rule).each |$key, $opts| {
+      firewalld_rich_rule {
+        default:
+          ensure => present,
+        ;
+        $key:
+          * => $opts,
       }
     }
   }
