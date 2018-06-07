fe8f8abb6f890fa6eeaef6b68a33250e7ea2ccb4
Geoff Williams <geoff@geoffwilliams.me.uk>
2018-06-07 13:56:49 +1000

    dox

--- manifests/linux/module_disable.pp
+++ manifests/linux/module_disable.pp
@@ -1,11 +1,17 @@
-# Redhat_tidy::Module_blacklist
+# R_Profile::Linux::Module_blacklist
 #
-# Blacklist/disable kernel modules by breaking them in /etc/modeprobe.d/blacklist.conf
+# Blacklist/disable kernel modules by breaking them in `/etc/modeprobe.d/blacklist.conf`. Kernel modules will be
+# completely disabled by this process as `modprobe` will try to load the related file. Since we cefinie the module as
+# `/bin/true`, it won’t be loaded.
 #
+# @see https://forge.puppet.com/puppetlabs/stdlib
+# @see https://linux-audit.com/kernel-hardening-disable-and-blacklist-linux-modules/
 #
+# @param modules Disable these kernel modules from loading
+# @param blacklist_file File to store the module blacklist in
 class r_profile::linux::module_disable(
-    Array[String] $modules = [],
-    $blacklist_file       = "/etc/modprobe.d/blacklist.conf",
+    Array[String] $modules        = [],
+    String        $blacklist_file = "/etc/modprobe.d/blacklist.conf",
 ) {
 
   file { $blacklist_file:
