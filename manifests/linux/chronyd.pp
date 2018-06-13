7b493db0a0ff4e8883e9c0e22304b0ac159c2eda
Geoff Williams <geoff@geoffwilliams.me.uk>
2018-06-13 16:16:52 +1000

    fixup bad cache dir for pdqtest, remove cached crap, better docs for ntp and chronyd, fix the name of chronyd from cronyd

---
+++ manifests/linux/chronyd.pp
@@ -0,0 +1,24 @@
+# R_profile::Linux::Chronyd
+#
+# Chronyd (formerly NTPd) support for Linux. No attempt is made to manage this service when running under docker since
+# the shared kernel makes this uncessary.
+#
+# @example Setup cronyd with the default list of servers
+#   include r_profile::linux::chronyd
+#
+# @example Hiera data to list of particular servers
+#   r_profile::linux::chronyd::servers:
+#     - '0.au.pool.ntp.org'
+#     - '1.au.pool.ntp.org'
+#
+# @param servers List of NTP servers to use
+class r_profile::linux::chronyd(
+    Array[String] $servers = ["pool.ntp.org"],
+) {
+
+  if $facts['virtual'] != "docker" {
+    class { 'chronyd':
+      servers => $servers,
+    }
+  }
+}
--- manifests/ntp.pp
+++ manifests/ntp.pp
@@ -1,28 +1,45 @@
 # R_profile::Ntp
 #
-# A more general-purpose NTP
+# NTP for Linux/unix based servers. Newer Linux systems can use `r_profile::linux::chronyd` to gain a more modern NTP
+# implementation.
+#
+# Windows users gain NTP by joining a domain. There is no support for standalone windows NTP yet
 #
 # Supports:
-# * AIX (in this class)
-# * Solaris (puppetlabs-ntp)
-# * Linux (puppetlabs-ntp)
+# * AIX
+# * Solaris
+# * Linux
+#
+# @see https://forge.puppet.com/puppetlabs/ntp
+#
+# @example setup default NTP client
+#   include r_profile::ntp
+#
+# @example Hiera data for setting the server(s) to use
+#   r_profile::ntp::servers:
+#     - time.windows.com
+#     - pool.ntp.org
+#
+# @example Hiera data to prevent trying to manage the package
+#   r_profile::ntp::package_manage: false
+#
+# @example Hiera data to prevent trying to manage the service
+#   r_profile::ntp::service_manage: false
 #
 # @param servers Array of servers to ask for the time
 # @param package_manage True if we should install ntp otherwise false
 # @param service_manage True if we should control the ntpd service otherwise false
 # @param service_name Name of the service to manage if we are instructed to do so
 class r_profile::ntp(
-    $servers                = hiera("r_profile::ntp::servers", undef),
-    Boolean $package_manage = hiera("r_profile::ntp::package_manage", true),
-    Boolean $service_manage = hiera("r_profile::ntp::service_manage", true),
-    $service_name           = hiera("r_profile::ntp::service_name", undef),
+    Optional[Array[String]] $servers        = undef,
+    Optional[Boolean]       $package_manage = undef,
+    Optional[Boolean]       $service_manage = undef,
 ) {
   if $facts['virtual'] != "docker" {
     class { "ntp":
       servers        => $servers,
       package_manage => $package_manage,
       service_manage => $service_manage,
-      service_name   => $service_name,
     }
   }
 }
