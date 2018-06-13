744fd5546bde64eeee790aeab32e2f1004ad58ff
Geoff Williams <geoff@geoffwilliams.me.uk>
2018-06-13 13:40:57 +1000

    remove separate directory parameter since file can do it all

--- manifests/file.pp
+++ manifests/file.pp
@@ -3,44 +3,33 @@
 # Support for managing files and directories
 #
 # @param files Hash of files to create (suitable for create_resources)
-# @param directories Hash of directories to create (suitable for create_resources)
 # @param process_template process the template() directive if found
 class r_profile::file(
-  $files             = hiera("r_profile::file::files", false),
-  $directories       = hiera("r_profile::file::directories", false),
+  $files             = hiera("r_profile::file::files", {}),
   $process_template = hiera("r_profile::file::process_template", false),
 ) {
 
-  $default_directory  = { "ensure" => "directory"}
-  $default_file       = { "ensure" => "file"}
-
-  if $files {
-    $files.each |$title, $opts| {
-      if $process_template {
-        if dig($opts, 'content') and $opts['content'] =~ /^template(.+)$/ {
-          # process the template and return the content
-          $template = regsubst($opts["content"], '^template\(([^)]+)\)$', '\1')
-          $temp = {
-            "content" => template($template)
-          }
-          # merge the hashes, allowing parsed content to override
-          $_opts = merge($opts, $temp)
+  $files.each |$title, $opts| {
+    if $process_template {
+      if dig($opts, 'content') and $opts['content'] =~ /^template(.+)$/ {
+        # process the template and return the content
+        $template = regsubst($opts["content"], '^template\(([^)]+)\)$', '\1')
+        $temp = {
+          "content" => template($template)
         }
-      } else {
-        $_opts = $opts
+        # merge the hashes, allowing parsed content to override
+        $_opts = merge($opts, $temp)
       }
-
-      file {
-        default:
-          * => $default_file,
-        ;
-        $title:
-          * => $_opts,
-      }
+    } else {
+      $_opts = $opts
     }
-  }
 
-  if $directories {
-    create_resources("file", $directories, $default_directory)
+    file {
+      default:
+        ensure => file,
+      ;
+      $title:
+        * => $_opts,
+    }
   }
 }
