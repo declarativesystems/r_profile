8a17266eee0cf3018a45e8e7123585875de3b4bf
Geoff Williams <geoff@geoffwilliams.me.uk>
2018-06-29 10:55:24 +1000

    added rsyslog; removed cache

--- manifests/linux/rsyslog_client.pp
+++ manifests/linux/rsyslog_client.pp
@@ -1,60 +1,42 @@
 # R_profile::Linux::Rsyslog
 #
-# Setup `rsyslog` client.
+# Setup `rsyslog` client:
+#   * Install Package
+#   * General settings
+#   * Managed logfiles
 #
-# After settings are present in the `/etc/rsyslog.conf` we also ensure any logfiles mentioned have the correct
-# permissions
-#
-# @see https://forge.puppet.com/puppet/rsyslog
+# @see https://forge.puppet.com/geoffwilliams/rsyslog_client
 #
 # @example Setup syslog client with settings from hiera
 #   include r_profile::linux::rsyslog
 #
-# @example specifying log files to manage in Hiera (we will manage permissons where `value` is a filename)
-#   r_profile::linux::rsyslog_client::logs:
-#     'cron_log':
-#       'key': 'cron.*'
-#       'value': '/var/log/cron'
-#     'emerg_log':
-#       'key': '*.emerg',
-#       'value': ':omusrmsg:*'
-#     'news_log':
-#       'key': 'uucp,news.crit'
-#       'value': '/var/log/spooler'
+# @example Hiera data for general rsyslog settings
+#   r_profile::linux::rsyslog_client::settings:
+#     - '$FileOwner': 'root'
+#     - '$FileGroup': 'root'
+#     - '$FileCreateMode': '0600'
+#     - '$DirOwner': 'root'
+#     - '$DirGroup': 'root'
+#     - '$DirCreateMode': '0750'
 #
-# @param settings Hash of settings to use. These will be passed directly to the `rsyslog::client` class so the supported
-#   settings are whatever this class supports @see https://github.com/voxpupuli/puppet-rsyslog/issues/64 for current
-#   example of how to setup a client. The exception to this is that the content of class parameter `logs` will be used
-#   to populate the `legacy_config` section. This allows the log files to be specified separately.
-# @param logs Hash of logs to manage. This maps to the `legacy_config` parameter on the `rsyslog::client` class. Where a
-#   `value` looks like a filename (see example) we will manage permissions on the file too.
-# @param log_group Group to grant ownership of log files to
-# @param log_mode File mode to give to log files
-# @param manage_permissions `true` to manage permissions as described above otherwise `false` to leave alone
+# @example Hiera data for rsyslog entries
+#   r_profile::linux::rsyslog_client::entries:
+#     'daemon.*': /var/log/daemon.log
+#     'syslog.*': /var/log/syslog
+#
+# @param settings Settings to insert into the `rsyslog.conf` file. Existing definitions will be
+#   replaced in-place. If a setting is added that doesn't already exist, it will be inserted
+#   into the file _from_ line `setting_insertion_line`. Settings will be processed in array
+#   order (see examples)
+# @param entries Hash of logging entries to create (see examples)
 class r_profile::linux::rsyslog_client(
-  Hash    $settings           = {},
-  Hash    $logs               = {},
-  String  $log_group          = "root",
-  String  $log_mode           = "0600",
-  Boolean $manage_permissions = false,
+  Array[Hash[String, Any]]  $settings = [],
+  Hash[String,String]       $entries  = {},
 ) {
 
-  if $manage_permissions {
-    $logs.each |$key, $opts| {
-      if $opts["value"] =~ /^\// {
-        file { $opts["value"]:
-          ensure => file,
-          owner  => "root",
-          group  => $log_group,
-          mode   => $log_mode,
-        }
-      }
-    }
-  }
-
-  class { 'rsyslog::client':
-    *             => $settings,
-    legacy_config => $logs,
+  class { 'rsyslog_client':
+    settings => $settings,
+    entries  => $entries,
   }
 
 }
--- spec/classes/linux/rsyslog_client_spec.rb
+++ spec/classes/linux/rsyslog_client_spec.rb
@@ -32,30 +32,20 @@ describe 'r_profile::linux::rsyslog_client' do
 
   let :params do
     {
-        "settings" => {
-            "modules"       => {
-                'imuxsock'  => {},
-                'imjournal' => {},
-            },
-            "global_config" => {
-                'workDirectory'             => {
-                    'value' => '/var/lib/rsyslog',
-                },
-            },
-        },
-        "logs" => {
-            'auth_priv_log'               => {
-                'key'   => 'authpriv.*',
-                'value' => '/var/log/secure',
-            },
-            'mail_log'                    => {
-                'key'   => 'mail.*',
-                'value' => '-/var/log/maillog',
-            },
-            'cron_log'                    => {
-                'key'   => 'cron.*',
-                'value' => '/var/log/cron',
-            },
+        "settings" => [
+            {'$FileOwner'      => 'root'},
+            {'$FileGroup'      => 'root'},
+            {'$FileCreateMode' => '0600'},
+            {'$DirOwner'       => 'root'},
+            {'$DirGroup'       => 'root'},
+            {'$DirCreateMode'  => '0750'},
+        ],
+        "entries" => {
+            "auth,user.*"                                                      => "/var/log/messages",
+            "kern.*"                                                           => "/var/log/kern.log",
+            "daemon.*"                                                         => "/var/log/daemon.log",
+            "syslog.*"                                                         => "/var/log/syslog",
+            "lpr,news,uucp,local0,local1,local2,local3,local4,local5,local6.*" => "/var/log/unused.log",
         }
     }
   end
