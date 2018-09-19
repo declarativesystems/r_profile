114cd0d2849f8ecbdb3a8585b4a9e895838f0e15
Geoff Williams <geoff@geoffwilliams.me.uk>
2018-09-19 17:49:07 +1000

    add missing docs

--- manifests/linux/auditd.pp
+++ manifests/linux/auditd.pp
@@ -13,7 +13,22 @@
 #     max_log_file_action: "rotate"
 #
 # @example Hiera data to manage rules
-#
+#   r_profile::linux::auditd::rules:
+#     10_date_and_time:
+#       content: |
+#         # data and time
+#         -a always,exit -F arch=b64 -S adjtimex -S settimeofday -k time-change
+#         -a always,exit -F arch=b32 -S adjtimex -S settimeofday -S stime -k time-change
+#         -a always,exit -F arch=b64 -S clock_settime -k time-change
+#         -a always,exit -F arch=b32 -S clock_settime -k time-change
+#         -w /etc/localtime -p wa -k time-change
+#     20_user_and_groups:
+#       content: |
+#         # users and groups
+#         -w /etc/group -p wa -k identity
+#         -w /etc/passwd -p wa -k identity
+#         -w /etc/gshadow -p wa -k identity
+#         -w /etc/shadow -p wa -k identity
 #
 # @param rules Hash of rules to enforce (see examples)
 # @param settings Hash or settings to apply to auditd (see examples)
